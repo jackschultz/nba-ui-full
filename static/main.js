541ba4f3ab68d79d9da6eb43398b5317331ff4d3
Jack Schultz <jackschultz23@gmail.com>
2019-11-03 12:44:22 -0600

    ability to sort, finally, and beginning of css

--- static/main.js
+++ static/main.js
@@ -53,14 +53,60 @@ Vue.component('demo-grid', {
 });
 
 
+
+
 var date = new Vue({
   delimiters: ['[[', ']]'],
   el: '#date',
-  data: { games: [],
-          statLines: {},
-          selectedStatLines: [],
-          projectionVersions: ['0.1-avg-dfn-min-03'],
-          selectedProjectionVersion: '0.1-avg-dfn-min-03' },
+  data: function() { 
+          var positions = ['PG', 'SG', 'SF', 'PF', 'C'];
+          var statLineColumns = ['Name', 'Team', 'Points', 'Minutes', 'Active', 'Salary', 'Positions', 'Minutes', 'Points'];
+          var sortOrders = {};
+          var sortKey = 'proj_minutes';
+          var selectedSite = 'fd';
+          var statLineColumnsTitles = ['Name', 'Team', 'Points', 'Minutes', 'Active', 'Salary', 'Positions', 'Minutes', 'Points'];
+          var statLineColumnsSortKeys = ['name', 'abbrv', 'points', 'minutes', 'sl_active', 'salary', 'positions', 'proj_minutes', 'proj_points'];
+          var statLineColumns = statLineColumnsTitles.map(function(e, i) {
+            return { 'title': e, 'sortKey': statLineColumnsSortKeys[i]};
+          });
+          statLineColumns.forEach(function (key) {
+            sortOrders[key.sortKey] = -1;
+          });
+          return { 
+            games: [],
+            statLines: {},
+            selectedStatLines: [],
+            projectionVersions: ['0.1-avg-dfn-min-05'],
+            selectedProjectionVersion: '0.1-avg-dfn-min-05' ,
+            statLineColumns: statLineColumns,
+            selectedSite: selectedSite,
+            sortKey: sortKey,
+            sortOrders: sortOrders,
+            statLineColumnsSortKeys: statLineColumnsSortKeys,
+          };
+  },
+  computed: {
+    filteredSelectedStatLines: function () {
+      var sortKey = this.sortKey;
+      var ssls = this.selectedStatLines;
+      var order = this.sortOrders[sortKey] || -1;
+      console.log(this.sortOrders);
+      if (sortKey) {
+        if ( ['points', 'positions', 'salary', 'proj_points'].includes(sortKey)) {
+          sortKey = this.selectedSite + '_' + sortKey;
+        }
+        console.log(sortKey);
+        if (ssls) {
+          ssls = ssls.slice().sort(function (a, b) {
+            a = a[sortKey];
+            b = b[sortKey];
+            return (a === b ? 0 : a > b ? 1 : -1) * order;
+          });
+        }
+      }
+      return ssls;
+    }
+  },
   mounted () {
     this.date = this.$el.getAttribute('data-date');
     this.starter(this.date);
@@ -94,19 +140,21 @@ var date = new Vue({
     selectedNewVersion: function(event) {
       this.selectedProjectionVersion = event.target.value;
       this.getStatLines(this.date);
-      /*
-      axios({
-        method: 'get',
-        url: 'http://localhost:5001/versions',
-        headers: { 'Content-type': 'application/json' }})
-        .then((response) => { this.projectionVersions = response.data }); */
     },
     starter: function(date) {
       this.getGames(date);
       this.getStatLines(date);
       this.getProjectionVersions(date);
-    }
-  },
-  created () {
+    },
+    sortBy: function (key) {
+      this.sortKey = key;
+      console.log(this.sortOrders);
+      var sortOrders = this.sortOrders;
+      this.statLineColumnsSortKeys.forEach(function(slcsk) {
+        if (slcsk !== key) { sortOrders[slcsk] = 1; } else { sortOrders[key] = sortOrders[key] * -1; };
+      });
+      this.sortOrders = sortOrders;
+      console.log(this.sortOrders);
+    },
   },
 });
